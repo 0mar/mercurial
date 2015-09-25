128644ec6713f0c8819a08886cb634d61a6970fe
0mar <omsrichardson@gmail.com>
2015-09-25 22:11:24 +0200

    Added documentation for classes in geometry

--- src/geometry.py
+++ src/geometry.py
@@ -13,6 +13,7 @@ class Coordinate(object):
     """
     Class that models a coordinate in Carthesian space.
     Used as a base class for points, velocities and sizes.
+    Implemented some basic 'magic' methods to facilitate the use of basic operators
     """
 
     def __init__(self, x):
@@ -49,6 +50,10 @@ class Coordinate(object):
         return "%s(%s)" % (self.type, ", ".join("%.2f" % f for f in self.array))
 
     def is_zero(self):
+        """
+        Check whether coordinates are within tolerance of zero point
+        :return: True if 2-norm of coordinate is smaller than epsilon
+        """
         return np.linalg.norm(self.array) < ft.EPS
 
 
@@ -65,6 +70,11 @@ class Size(Coordinate):
             raise ValueError("Negative size specified")
 
     def random_internal_point(self):
+        """
+        Provides a random internal point within the size
+        :return: Point with positive coordinates both smaller than size
+        """
+
         return Point(np.array([random.random() * dim for dim in self.array]))
 
 
@@ -89,6 +99,9 @@ class Velocity(Coordinate):
 
 
 class Interval(object):
+    """
+    Class that models an interval
+    """
     def __init__(self, coords):
         self.array = np.array(coords)
         self.length = coords[1] - coords[0]
@@ -96,6 +109,10 @@ class Interval(object):
             raise ValueError("Interval start larger than interval end")
 
     def random(self):
+        """
+        Returns a random double within the interval
+        :return:
+        """
         return np.random.random() * self.length + self.array[0]
 
     begin = property(lambda s: s[0])
@@ -111,7 +128,7 @@ class Interval(object):
         return "Interval %s" % self.array
 
 
-# Moeten ook naar Points ipv arrays toe
+# Todo: Moeten ook naar Points ipv arrays toe
 class LineSegment(object):
     def __init__(self, coords):
         self.array = np.array(coords)
@@ -121,20 +138,25 @@ class LineSegment(object):
     begin = property(lambda s: s[0])
     end = property(lambda s: s[1])
 
-    @staticmethod
-    def path_norm(path, t):
-        return np.linalg.norm(path[0] + t * (path[1] - path[0]), ord=1)
-
-    def get_point(self, value:float):
+    def get_point(self, value):
+        """
+        Regarding the line segment as a 1D parameter equation with domain [0,1],
+         this method returns the coordinates for parameter value
+        :param value: parameter in [0,1]
+        :return: corresponding Point on line segment
+        """
+        if 0 >= value <= 1:
+            raise ValueError("Value %.2f must lie between 0 and 1" % value)
         return self.begin + value * (self.end - self.begin)
 
     def crosses_obstacle(self, obstacle, open_sets=False):
         """
         Checks whether the line crosses the rectangular obstacle.
-        Implemented more efficiently than its counterpart, and even more precise.
-        :param obstacle: The rectangular obstacle to be checked
-        :param strict: Whether the line passes only through the obstacle (open_sets = True)
-         or also through the boundary (open_sets = False)
+        Novel implementation based on hyperplanes and other linear algebra (*proud*)
+        :param obstacle: The rectangular obstacle under consideration
+        :param open_sets: Whether it counts as an intersection
+         if the line passes inside of the obstacle (open_sets = True)
+         or also counts when it passes through the obstacle boundary (open_sets = False)
         :return: True if line crosses obstacle, false otherwise
         """
         line_array = np.array([self.begin, self.end])
@@ -176,37 +198,64 @@ class LineSegment(object):
         return "LineSegment from %s to %s" % (Point(self.begin), Point(self.end))
 
 
-# Wrapper around list of line segments
 class Path(object):
+    """
+    Wrapper around a sequence of line segments.
+    """
     def __init__(self, line_segment_list):
+        if not isinstance(line_segment_list, list):
+            raise TypeError("Line segment list must be list")
         self.list = line_segment_list
         for i in range(len(line_segment_list) - 1):
-            self._check_connectivity(line_segment_list[i], line_segment_list[i + 1])
+            self.check_lines_connected(line_segment_list[i], line_segment_list[i + 1])
 
     @property
     def length(self):
+        """
+        Summed length of all line segments
+        :return: sum of line segments 2-norms
+        """
         return sum([line.length for line in self.list])
 
     def __len__(self):
         return len(self.list)
 
-    def _check_connectivity(self, ls1: LineSegment, ls2: LineSegment):
+    @staticmethod
+    def check_lines_connected(ls1: LineSegment, ls2: LineSegment):
+        """
+        Returns true if last line end connects (within epsilon accuracy) to new line begin
+        :param ls1: Line segment whose end matches ls2's begin
+        :param ls2: Line segment whose begin matches ls1's end
+        :return: True if lines connect, False otherwise
+        """
         connected = np.allclose(ls1.end, ls2.begin)
         if not connected:
             raise AssertionError("%s & %s do not connect" % (ls1, ls2))
 
     def __iadd__(self, other):
+        """
+        Appends 2 paths to each other.
+        :param other: path object (which start should connect to this end)
+        :return: None
+        :raise: AssertionError if paths do not connect
+        """
         if self.list:
-            self._check_connectivity(self.list[-1], other[0])
-        self.list += other
+            Path.check_lines_connected(self.list[-1], other[0])
+            self.list += other
 
     def __getitem__(self, item):
         return self.list[item]
 
     def append(self, other):
+        """
+        Append line segment to this path. Path has to have at least one line segment.
+        :param other: Line segment which should connect to the last line segment in this path, or Point
+        :return: None
+        :raise: AssertionError if path is empty, AttributeError if 'other' has wrong type.
+        """
         if isinstance(other, LineSegment):
             if self.list:
-                self._check_connectivity(self.list[-1], other)
+                Path.check_lines_connected(self.list[-1], other)
             self.list.append(other)
         elif isinstance(other, Point):
             if not self.list:
@@ -218,12 +267,23 @@ class Path(object):
             raise AttributeError("Object appended to Path must be LineSegment or Point, not %s" % other)
 
     def pop_next_segment(self):
+        """
+        Returns the first line segment of the path and removes it from the path list.
+        :return: Line segment
+        """
         return self.list.pop(0)
 
     def __bool__(self):
+        """
+        :return: True if Path has any elements, False otherwise.
+        """
         return bool(self.list)
 
     __nonzero__ = __bool__
 
     def __str__(self):
+        """
+        String representation of collected line segment
+        :return: Joined string representation for each line segment
+        """
         return "Path:\n%s" % "\n".join([str(ls) for ls in self.list])
