63ed11651df085b1fcc77f7359da02a0e659d128
0mar <omsrichardson@gmail.com>
2015-10-29 21:29:34 +0100

    Fixed orientation bug ; added default save file; adapted dummy objects

--- src/create_scene.py
+++ src/create_scene.py
@@ -39,40 +39,40 @@ class SceneCreator:
 
         self.obstacle_list = []
         self.new_object_holder = 0
-        self.obs_start = self.obs_end = None
+        self.draw_start = self.draw_end = None
         self.exit_list = []
 
     def start_drawing(self, event):
         ft.debug(self.object_status.get())
         self.draw_status = SceneCreator.DrawStatus.drawing
-        self.obs_start = (event.x, event.y)
+        self.draw_start = (event.x, event.y)
         self.window.bind('<Motion>', self.motion)
         self.window.bind('<Button-1>', self.stop_drawing)
-        ft.debug("Started drawing. Start location of obstacle: %s" % str(self.obs_start))
+        ft.debug("Started drawing. Start location of obstacle: %s" % str(self.draw_start))
 
     def stop_drawing(self, event):
         ft.debug("stopped drawing")
         self.window.unbind('<Motion>')
         self.draw_status = SceneCreator.DrawStatus.idle
-        self.obs_end = (event.x, event.y)
-        ft.debug("End of obstacle: %s" % str(self.obs_end))
+        self.draw_end = (event.x, event.y)
+        ft.debug("End of obstacle: %s" % str(self.draw_end))
         new_obstacle = self.create_new_obstacle()
-        self.obs_start = self.obs_end = None
+        self.draw_start = self.draw_end = None
         self.window.bind('<Button-1>', self.start_drawing)
         self.draw_obstacle(new_obstacle)
 
     def draw_scene(self):
         if self.draw_status == SceneCreator.DrawStatus.drawing:
-            self.redraw_new_obstacle(self.obs_start, self.obs_end)
+            self.redraw_new_obstacle(self.draw_start, self.draw_end)
 
     def draw_obstacle(self, obstacle):
-        real_coords = tuple([i * self.size[0] for i in obstacle.start + obstacle.end])
-        ft.debug("Drawing obstacle on %s" % str(real_coords))
+        draw_coords = self.convert_relative_coordinate(obstacle.start) + self.convert_relative_coordinate(obstacle.end)
+        ft.debug("Drawing obstacle on %s" % str(draw_coords))
         if SceneCreator.ObjectStatus(self.object_status.get()) == SceneCreator.ObjectStatus.obstacle:
             color = 'gray'
         else:
             color = 'red'
-        self.canvas.create_rectangle(real_coords, fill=color)
+        self.canvas.create_rectangle(draw_coords, fill=color)
 
     def redraw_new_obstacle(self, start, end):
         if self.new_object_holder:
@@ -82,24 +82,27 @@ class SceneCreator:
     def motion(self, event):
         assert self.draw_status == SceneCreator.DrawStatus.drawing
         cur_pos = (event.x, event.y)
-        self.redraw_new_obstacle(self.obs_start, cur_pos)
+        self.redraw_new_obstacle(self.draw_start, cur_pos)
 
     def create_new_obstacle(self):
-        begin_x = min(self.obs_start[0], self.obs_end[0]) / self.size[0]
-        begin_y = min(self.obs_start[1], self.obs_end[1]) / self.size[0]
-        end_x = max(self.obs_start[0], self.obs_end[0]) / self.size[0]
-        end_y = max(self.obs_start[1], self.obs_end[1]) / self.size[0]
+        begin_x = min(self.draw_start[0], self.draw_end[0]) / self.size[0]
+        begin_y = 1 - max(self.draw_start[1], self.draw_end[1]) / self.size[1]
+        end_x = max(self.draw_start[0], self.draw_end[0]) / self.size[0]
+        end_y = 1 - min(self.draw_start[1], self.draw_end[1]) / self.size[1]
         if SceneCreator.ObjectStatus(self.object_status.get()) == SceneCreator.ObjectStatus.obstacle:
-            new_obstacle = Obstacle((begin_x, begin_y), (end_x, end_y))
+            new_obstacle = Obstacle((begin_x, begin_y), (end_x - begin_x, end_y - begin_y))
             self.obstacle_list.append(new_obstacle)
             return new_obstacle
         else:
-            new_exit = Exit((begin_x, begin_y), (end_x, end_y))
+            new_exit = Exit((begin_x, begin_y), (end_x - begin_x, end_y - begin_y))
             self.exit_list.append(new_exit)
             return new_exit
 
     def ask_save(self):
         save_name = tkinter.simpledialog.askstring("Save file", "Obstacle file name")
+        if not save_name:
+            save_name = 'latest_scene'
+        save_name = save_name.replace('/', '_')
         self.save(save_name)
 
     def save(self, save_name):
@@ -107,20 +110,22 @@ class SceneCreator:
         for number, obstacle in enumerate(self.obstacle_list):
             name = "Obstacle#%d" % number
             obstacle_data["obstacles"].append({"name": name, "begin": obstacle.start, "size": obstacle.size})
-
-        for number, obstacle in enumerate(self.exit_list):
+        for number, exit_obstacle in enumerate(self.exit_list):
             name = "Exit#%d" % number
-            obstacle_data["exits"].append({"name": name, "begin": obstacle.start, "size": obstacle.size})
+            obstacle_data["exits"].append({"name": name, "begin": exit_obstacle.start, "size": exit_obstacle.size})
         with open('../scenes/%s.json' % save_name, 'w') as object_file:
             object_file.write(json.dumps(obstacle_data, indent=4, sort_keys=True))
 
+    def convert_relative_coordinate(self, coord):
+        return coord[0] * self.size[0], (1 - coord[1]) * self.size[1]
+
 
 class Obstacle:
-    def __init__(self, start, end):
-        ft.debug("Creating new obstacle with start %s and end %s" % (str(start), str(end)))
-        self.start = start
-        self.end = end
-        self.size = (end[0] - start[0], end[1] - start[1])
+    def __init__(self, begin, size):
+        ft.debug("Creating new obstacle with start %s and size %s" % (str(begin), str(size)))
+        self.start = begin
+        self.size = size
+        self.end = (begin[0] + size[0], begin[1] + size[1])
 
     def __repr__(self):
         return "obstacle#(%d,%d)-(%d,%d)" % (self.start[0], self.start[1], self.end[0], self.end[1])
