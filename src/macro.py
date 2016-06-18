6037d0b8013e40427ce0917d35e9cf6cf165304f
0mar <omsrichardson@gmail.com>
2016-06-18 16:31:21 +0200

    Introduced split between macroscopic and microscopic planning. Current version is broken due to lingering cython calls

---
+++ src/macro.py
@@ -0,0 +1,204 @@
+__author__ = 'omar'
+import matplotlib
+
+matplotlib.use('TkAgg')
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from fortran_modules.micro_macro import comp_dens_velo
+from fortran_modules.pressure_computer import compute_pressure
+from scalar_field import ScalarField as Field
+import cvxopt
+import functions as ft
+from geometry import Point
+
+class MacroTransporter:
+    """
+    Class responsible for the macroscopic calculations of the crowd.
+    Main tasks:
+    1. Interpolating velocity on grid points
+    2. Computing pressure
+    3. Adapting global velocity field with pressure gradient
+    4. Adapting individual velocity by global velocity field.
+    """
+
+    def __init__(self, scene, show_plot, apply_interpolation, apply_pressure):
+        """
+        Constructs a grid computer, responsible for the continuum calculations.
+        The grid computer takes several parameters in its constructor.
+        Numerical parameters are specified in the parameters file.
+        :param scene: Scene on which we make the computations
+        :param show_plot: enable matplotlib plotting of discrete fields
+        :param apply_interpolation: impose group velocity on pedestrians
+        :param apply_pressure: impose pressure on velocity field (and on pedestrians)
+        :return: Grid computer object.
+        """
+        self.scene = scene
+        prop_dx = scene.config['general'].getfloat('cell_size_x')
+        prop_dy = scene.config['general'].getfloat('cell_size_y')
+        self.grid_dimension = (self.scene.size.array / (prop_dx, prop_dy)).astype(int)
+        self.dx, self.dy = self.scene.size.array / self.grid_dimension
+        self.dt = self.scene.dt
+        self.packing_factor = scene.config['dynamic'].getfloat('packing_factor')
+        self.max_density = 2 * self.packing_factor / \
+                           (np.sqrt(3) * self.scene.core_distance ** 2)
+        cvxopt.solvers.options['show_progress'] = False
+        self.smoothing_length = 2 * self.scene.core_distance
+        self.basis_A = self.basis_v_x = self.basis_v_y = None
+        self._last_solution = None
+        self.show_plot = show_plot
+        self.apply_interpolation = apply_interpolation or apply_pressure
+        self.apply_pressure = apply_pressure
+        dx, dy = self.dx, self.dy
+        shape = self.grid_dimension
+        self.obstacle_correction = np.zeros(shape)
+        self._get_obstacle_coverage()
+        self.density_field = Field(shape, Field.Orientation.center, 'density', (dx, dy))
+        self.v_x = Field(shape, Field.Orientation.center, 'velocity_x', (dx, dy))
+        self.v_y = Field(shape, Field.Orientation.center, 'velocity_y', (dx, dy))
+        self.pressure_field = Field((shape[0] + 2, shape[1] + 2), Field.Orientation.center, 'pressure', (dx, dy))
+        # If beneficial, we could employ a staggered grid
+
+        if self.show_plot:
+            # Plotting hooks
+            f, self.graphs = plt.subplots(2, 2)
+            plt.show(block=False)
+
+    def _get_obstacle_coverage(self, precision=2):
+        """
+        Compute the fraction of the cells covered with obstacles
+        Since an exact computation involves either big shot linear algebra
+        or too much case distinctions, we sample the cell space.
+        :param precision: row number of samples per cell. More samples, more precision.
+        :return: an array approximating the inaccessible space in the cells
+        """
+        ft.log("Started sampling objects (%d checks)"%(precision**2*self.grid_dimension[0]*self.grid_dimension[1]))
+        obstacle_coverage = np.zeros(self.obstacle_correction.shape)
+        num_samples = (precision, precision)
+        size = Point([self.dx, self.dy])
+        for row, col in np.ndindex(obstacle_coverage.shape):
+            covered_samples = 0
+            begin = Point([row * self.dx, col * self.dy])
+            for i, j in np.ndindex(num_samples):
+                for obstacle in self.scene.obstacle_list:
+                    if not obstacle.accessible:
+                        if Point(begin.array + [i + 0.5, j + 0.5] / np.array(num_samples)*size.array) in obstacle:
+                            covered_samples += 1
+                            break
+            obstacle_coverage[row, col] = covered_samples / (num_samples[0] * num_samples[1])
+        # Correct the fully covered entries
+        obstacle_coverage[obstacle_coverage == 1] = 0.8
+        self.obstacle_correction = 1 / (1 - obstacle_coverage)
+        ft.log("Finished sampling objects")
+
+    def plot_grid_values(self):
+        """
+        Plot the density, velocity field, pressure, and pressure gradient.
+        Plots are opened in a separate window and automatically updated.
+        :return:
+        """
+        for graph in self.graphs.flatten():
+            graph.cla()
+        self.graphs[0, 0].imshow(np.rot90(self.density_field.array))
+        self.graphs[0, 0].set_title('Density')
+        self.graphs[1, 0].imshow(np.rot90(self.pressure_field.array))
+        self.graphs[1, 0].set_title('Pressure')
+        self.graphs[0, 1].quiver(self.v_x.mesh_grid[0], self.v_x.mesh_grid[1], self.v_x.array, self.v_y.array, scale=1,
+                                 scale_units='xy')
+        self.graphs[0, 1].set_title('Velocity field')
+        self.graphs[1, 1].quiver(self.v_x.mesh_grid[0], self.v_x.mesh_grid[1],
+                                 self.pressure_field.gradient('x')[:, 1:-1],
+                                 self.pressure_field.gradient('y')[1:-1, :], scale=1, scale_units='xy')
+        self.graphs[1, 1].set_title('Pressure gradient')
+        plt.show(block=False)
+
+    def compute_pressure(self):
+        pressure = compute_pressure(self.density_field.array + 0.1, self.v_x.array, self.v_y.array,
+                                    self.dx, self.dy, self.dt, self.max_density)
+        dim_p = np.reshape(pressure, (self.grid_dimension[0], self.grid_dimension[1]), order='F')
+        padded_dim_p1 = np.pad(dim_p, (1, 1), 'constant', constant_values=1)
+        self.pressure_field.update(padded_dim_p1)
+
+
+    def adjust_velocity(self):
+        """
+        Adjusts the velocity field for the pressure gradient. We pad the pressure with an extra boundary
+        so that the gradient is defined everywhere.
+        :return: None
+        """
+        # Not using the update method.
+        time1 = time.time()
+        well_shaped_x_grad = self.pressure_field.gradient('x')[:, 1:-1]
+        well_shaped_y_grad = self.pressure_field.gradient('y')[1:-1, :]
+        self.v_x.array -= well_shaped_x_grad
+        self.v_y.array -= well_shaped_y_grad
+        ft.debug("Step 4: Applying pressure %.4f"%(time.time()-time1))
+
+
+    def interpolate_pedestrians(self):
+        """
+        Method that reconverts the velocity field to individual pedestrian positions.
+        Input are the velocity x/y fields. We use a bivariate spline interpolation method
+        to interpolate the velocities at the pedestrians position.
+        These velocities are then weighed to the densities, normalized
+        and added to the velocity field.
+        :return: None
+        """
+        v_x_func = self.v_x.get_interpolation_function()
+        v_y_func = self.v_y.get_interpolation_function()
+        dens_func = self.density_field.get_interpolation_function()
+        solved_v_x = v_x_func.ev(self.scene.position_array[:, 0], self.scene.position_array[:, 1])
+        solved_v_y = v_y_func.ev(self.scene.position_array[:, 0], self.scene.position_array[:, 1])
+        local_dens = np.minimum(dens_func.ev(self.scene.position_array[:, 0], self.scene.position_array[:, 1]),
+                                self.max_density)
+        solved_velocity = np.hstack((solved_v_x[:, None], solved_v_y[:, None]))
+        self.scene.velocity_array = self.scene.velocity_array + local_dens[:, None] / self.max_density * (
+        solved_velocity - self.scene.velocity_array) + ft.EPS
+        self.scene.velocity_array /= \
+            np.linalg.norm(self.scene.velocity_array, axis=1)[:, None] / (self.scene.max_speed_array[:, None] + ft.EPS)
+
+    def step(self):
+        """
+        Performs one time step for the fluid simulator.
+        Computes and interpolates the grid fields (density and velocity)
+        Solves the LCP to compute the pressure field
+        Adjusts the velocity field according to the pressure
+        Adjusts the velocities of the pedestrians according to the velocity field
+        :return: None
+        """
+        if self.apply_interpolation or self.show_plot:
+            n_x, n_y = self.grid_dimension
+            dx, dy = self.scene.size.array / self.grid_dimension
+            density_field, v_x, v_y = comp_dens_velo(self.scene.position_array, self.scene.velocity_array,
+                                                     self.scene.active_entries, n_x, n_y, dx, dy, self.smoothing_length)
+            self.density_field.update(self.obstacle_correction * density_field)
+            self.v_x.update(v_x)
+            self.v_y.update(v_y)
+            ft.debug("Max allowed density: %.4f"%self.max_density)
+            ft.debug("Max observed density: %.4f"%np.max(self.density_field.array))
+
+        if self.apply_interpolation:
+
+            if self.apply_pressure:
+                self.compute_pressure()
+                self.adjust_velocity()
+                if self.show_plot:
+                    self.plot_grid_values()
+                # self.compute_pressure()
+            # print("Time took: %.4f" % (time.time() - time1))
+            self.interpolate_pedestrians()
+
+    @staticmethod
+    def weight_function(array, smoothing_length=1):
+        """
+        Using the Wendland kernel to determine the interpolation weight
+        Calculation is performed in two steps to take advantage of numpy's speed
+        :param array: Array of distances to apply the kernel on.
+        :param smoothing_length: Steepness factor (standard deviation) of kernel
+        :return: Weights of interpolation
+        """
+        array /= smoothing_length
+        norm_constant = 7. / (4 * np.pi * smoothing_length * smoothing_length)
+        first_factor = np.maximum(1 - array / 2, 0)
+        weight = first_factor ** 4 * (1 + 2 * array)
+        return weight * norm_constant
---
+++ src/micro.py
@@ -0,0 +1,254 @@
+import numpy as np
+import math
+import operator
+import functions as ft
+from geometry import Point
+from scalar_field import ScalarField as Field
+
+
+class MicroTransporter:
+
+    def __init__(self, scene, show_plot=False):
+        """
+        Initializes a dynamic planner object. Takes a scene as argument.
+        Parameters are initialized in this constructor, still need to be validated.
+        :param scene: scene object to impose planner on
+        :return: dynamic planner object
+        """
+        # Initialize depending on scene or on grid_computer?
+        self.scene = scene
+        self.config = scene.config
+        prop_dx = self.config['general'].getfloat('cell_size_x')
+        prop_dy = self.config['general'].getfloat('cell_size_y')
+        self.grid_dimension = tuple((self.scene.size.array / (prop_dx, prop_dy)).astype(int))
+        self.dx, self.dy = self.scene.size.array / self.grid_dimension
+        self.show_plot = show_plot
+
+        self.path_length_weight = self.config['dynamic'].getfloat('path_length_weight')
+        self.time_weight = self.config['dynamic'].getfloat('time_weight')
+        self.discomfort_field_weight = self.config['dynamic'].getfloat('discomfort_weight')
+
+        self.all_cells = {(i, j) for i, j in np.ndindex(self.grid_dimension)}
+        self.exit_cell_set = set()
+        self.obstacle_cell_set = set()
+        self.part_obstacle_cell_dict = dict()  # Immediately store the fractions
+        dx, dy = self.dx, self.dy
+        shape = self.grid_dimension
+
+        self.potential_field = Field(shape, Field.Orientation.center, 'potential', (dx, dy))
+        self.discomfort_field = Field(shape, Field.Orientation.center, 'discomfort', (dx, dy))
+        self.obstacle_discomfort_field = np.zeros(shape)
+        self._compute_obstacle_discomfort()
+
+        self.pot_grad_x = Field(shape, Field.Orientation.vertical_face, 'pot_grad_x', (dx, dy))
+        self.pot_grad_y = Field(shape, Field.Orientation.horizontal_face, 'pot_grad_y', (dx, dy))
+        self.grad_x_func = self.grad_y_func = None
+        self.unit_field_dict = {}
+        for direction in ft.VERTICAL_DIRECTIONS:
+            self.unit_field_dict[direction] = Field(shape, Field.Orientation.horizontal_face,
+                                                    'Unit field %s' % direction, (dx, dy))
+
+        for direction in ft.HORIZONTAL_DIRECTIONS:
+            self.unit_field_dict[direction] = Field(shape, Field.Orientation.vertical_face, 'Unit field %s' % direction,
+                                                    (dx, dy))
+
+        if not ft.VERBOSE:
+            np.seterr(invalid='ignore')
+        self.obtain_potential_field()
+
+    def obtain_potential_field(self):
+        self._compute_initial_interface()
+        for direction in ft.DIRECTIONS:
+            self._compute_unit_cost_field(direction)
+        self.compute_potential_field()
+        self._compute_potential_gradient()
+        self.grad_x_func = self.pot_grad_x.get_interpolation_function()
+        self.grad_y_func = self.pot_grad_y.get_interpolation_function()
+
+    def _compute_initial_interface(self):
+        """
+        Compute the initial zero interface; a potential field with zeros on exits
+        and infinity elsewhere. Stores inside object.
+        This method also validates exits. If no exit is found, the method raises an error.
+        :return: None
+        """
+        self.initial_interface = np.ones(self.grid_dimension) * np.inf
+        valid_exits = {goal: False for goal in self.scene.exit_list}
+        goals = self.scene.exit_list
+        for i, j in np.ndindex(self.grid_dimension):
+            cell_center = Point([(i + 0.5) * self.dx, (j + 0.5) * self.dy])
+            for goal in goals:
+                if cell_center in goal:
+                    self.initial_interface[i, j] = 0
+                    valid_exits[goal] = True
+                    self.exit_cell_set.add((i, j))
+            for obstacle in self.scene.obstacle_list:
+                if not obstacle.accessible:
+                    if cell_center in obstacle:
+                        # self.initial_interface[i,j] = np.inf
+                        self.obstacle_cell_set.add((i, j))
+        if not any(valid_exits.values()):
+            raise RuntimeError("No cell centers in exit. Redo the scene")
+        if not all(valid_exits.values()):
+            ft.warn("%s not properly processed" % "/"
+                    .join([repr(goal) for goal in self.scene.exit_list if not valid_exits[goal]]))
+
+    def _compute_obstacle_discomfort(self):
+        """
+        Create a layer of discomfort around obstacles to repel pedestrians from those locations.
+        """
+        for (i, j) in np.ndindex(self.discomfort_field.array.shape):
+            location = np.array([self.discomfort_field.x_range[i], self.discomfort_field.y_range[j]])
+            if not self.scene.is_accessible(Point(location)):
+                self.obstacle_discomfort_field[i - 1:i + 2, j - 1:j + 2] = 1
+
+    def _exists(self, index, max_index=None):
+        """
+        Checks whether an index exists within the max_index dimensions
+        :param index: 2D index tuple
+        :param max_index: max index tuple
+        :return: true if lower than tuple, false otherwise
+        """
+        if not max_index:
+            max_index = self.grid_dimension
+        return (0 <= index[0] < max_index[0]) and (0 <= index[1] < max_index[1])
+
+    def _compute_unit_cost_field(self, direction):
+        """
+        Compute the unit cost vector field in the provided direction
+        Updates the class unit cost scalar field
+        :return: None
+        """
+        alpha = self.path_length_weight
+        beta = self.time_weight
+        gamma = self.discomfort_field_weight
+        f = 1
+        g = self.discomfort_field.with_offset(direction)
+        self.unit_field_dict[direction].update(alpha + (f + beta + gamma * g) / (f + ft.EPS))
+
+    def compute_potential_field(self):
+        """
+        Compute the potential field as a function of the unit cost.
+        Also computes the gradient of the potential field
+        Implemented using the fast marching method
+
+        Potential is initialized with zero on exits, and a fixed high value on inaccessible cells.
+        :return:
+        """
+        opposites = {'left': 'right', 'right': 'left', 'up': 'down', 'down': 'up'}
+        # This implementation is allowed to be naive: it's costly and should be implemented in FORTRAN
+        # But maybe do a heap structure first?
+        potential_field = self.initial_interface.copy()
+        known_cells = self.exit_cell_set.copy()
+        unknown_cells = self.all_cells - known_cells - self.obstacle_cell_set
+        # All the inaccessible cells are not required.
+
+        def get_new_candidate_cells(new_known_cells):  # Todo: Finalize list/set interfacing
+            new_candidate_cells = set()
+            for cell in new_known_cells:
+                for direction in ft.DIRECTIONS.values():
+                    nb_cell = (cell[0] + direction[0], cell[1] + direction[1])
+                    if self._exists(nb_cell) and nb_cell not in known_cells and nb_cell not in self.obstacle_cell_set:
+                        new_candidate_cells.add(nb_cell)
+            return new_candidate_cells
+
+        def compute_potential(cell):
+            """
+            Computes the potential in one cell, using potential in neighbouring cells.
+            """
+            # Find the minimal directions along a grid cell.
+            # Assume left and below are best, then overwrite with right and up if they are better
+            neighbour_pots = {direction: np.Inf for direction in ft.DIRECTIONS}
+
+            hor_potential = ver_potential = 0
+            hor_cost = ver_cost = np.Inf
+
+            for direction in ft.DIRECTIONS:
+                normal = ft.DIRECTIONS[direction]
+                # numerical direction
+                nb_cell = (cell[0] + normal[0], cell[1] + normal[1])
+                if not self._exists(nb_cell):
+                    continue
+                pot = potential_field[nb_cell]
+                # potential in that neighbour field
+                if direction == 'right':
+                    face_index = (nb_cell[0] - 1, nb_cell[1])
+                elif direction == 'up':
+                    face_index = (nb_cell[0], nb_cell[1] - 1)
+                    # Unit cost values are defined w.r.t faces, not cells!
+                else:
+                    face_index = nb_cell
+                cost = self.unit_field_dict[opposites[direction]].array[face_index]
+                # Cost to go from there to here
+                neighbour_pots[direction] = pot + cost
+                # total potential
+                if neighbour_pots[direction] < neighbour_pots[opposites[direction]]:
+                    if direction in ft.HORIZONTAL_DIRECTIONS:
+                        hor_potential = pot
+                        hor_cost = cost
+                        # lowest in horizontal direction
+                    elif direction in ft.VERTICAL_DIRECTIONS:
+                        ver_potential = pot
+                        ver_cost = cost
+                        # lowest in vertical direction
+                    else:
+                        raise ValueError("Direction unknown")
+            # Coefficients of quadratic equation
+            a = 1 / hor_cost ** 2 + 1 / ver_cost ** 2
+            b = -2 * (hor_potential / hor_cost ** 2 + ver_potential / ver_cost ** 2)
+            c = (hor_potential / hor_cost) ** 2 + (ver_potential / ver_cost) ** 2 - 1
+
+            D = b ** 2 - 4 * a * c
+            x_high = (2 * c) / (-b - math.sqrt(D))
+            # Might not be obvious, but why we take the largest root is found in report.
+            return x_high
+
+        candidate_cells = {cell: compute_potential_cy(cell, potential_field, self.unit_field_dict, opposites)
+                           for cell in get_new_candidate_cells(known_cells)}
+
+        new_candidate_cells = get_new_candidate_cells(known_cells)
+        while unknown_cells:
+            for candidate_cell in new_candidate_cells:
+                if False:
+                    potential = compute_potential(candidate_cell)
+                else:
+                    potential = compute_potential_cy(candidate_cell, potential_field, self.unit_field_dict, opposites)
+                    #Todo: Push to fortran or back to python
+                candidate_cells[candidate_cell] = potential
+            sorted_candidates = sorted(candidate_cells.items(), key=operator.itemgetter(1))  # Todo: Can we reuse this?
+            best_cell = sorted_candidates[0][0]
+            min_potential = candidate_cells.pop(best_cell)
+            potential_field[best_cell] = min_potential
+            unknown_cells.remove(best_cell)
+            known_cells.add(best_cell)
+            new_candidate_cells = get_new_candidate_cells({best_cell})
+        self.potential_field.update(potential_field)
+
+    def _compute_potential_gradient(self):
+        """
+        Compute a gradient component approximation of the provided field.
+        Only computed for face fields.
+        Gradient approximation is computed using a central difference scheme
+        """
+        left_field = self.potential_field.array[:-1, :]
+        right_field = Field.get_with_offset(self.potential_field.array, 'right')
+        assert self.pot_grad_x.array.shape == left_field.shape
+        self.pot_grad_x.update((right_field - left_field) / self.dx)
+        self.pot_grad_x.array[np.logical_not(np.isfinite(self.pot_grad_x.array))] = 0
+        down_field = self.potential_field.array[:, :-1]
+        up_field = Field.get_with_offset(self.potential_field.array, 'up')
+        assert self.pot_grad_y.array.shape == up_field.shape
+        self.pot_grad_y.update((up_field - down_field) / self.dy)
+        self.pot_grad_y.array[np.logical_not(np.isfinite(self.pot_grad_y.array))] = 0
+
+    def compute_velocities(self):
+        """
+        Interpolates the potential gradients for this time step and computes the velocities.
+        :return: None
+        """
+
+        solved_grad_x = self.grad_x_func.ev(self.scene.position_array[:, 0], self.scene.position_array[:, 1])
+        solved_grad_y = self.grad_y_func.ev(self.scene.position_array[:, 0], self.scene.position_array[:, 1])
+        solved_grad = np.hstack([solved_grad_x[:, None], solved_grad_y[:, None]])
+        self.scene.velocity_array = - self.scene.max_speed_array[:, None] * solved_grad / \
+                                    np.linalg.norm(solved_grad + ft.EPS, axis=1)[:, None]
