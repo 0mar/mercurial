c307fc9fff338c0a1a1b3535db3ab69fb1e9bba4
0mar <omsrichardson@gmail.com>
2016-06-18 12:22:33 +0200

    Removed Cython from source code

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-from distutils.core import setup
 import os
 from numpy.distutils.core import Extension
 from numpy.distutils.core import setup as npsetup
-from Cython.Build import cythonize
 
 mde = Extension(name='mde', sources=['src/fortran/mde.f90'])
 micro_macro = Extension(name='micro_macro', sources=['src/fortran/micro_macro.f90'])
@@ -10,17 +8,11 @@ pressure_computer = Extension(name='pressure_computer', sources=['src/fortran/pr
                                                                  'src/fortran/sparse_modules.f90'])
 
 if __name__ == "__main__":
+    # Create a folder for images
     if not os.path.exists('images'):
         os.makedirs('images')
+    # Build the Fortran modules
     npsetup(name='FORTRAN modules',
             description="FORTRAN modules for Mercurial",
             author="Omar Richardson",
             ext_modules=[mde, micro_macro, pressure_computer])
-
-    # Cython will be removed in the next version
-    cython_path = 'src/cython_modules'
-    module_list = ["grid_computer_cy.pyx", "mde_cy.pyx", "dynamic_planner_cy.pyx"]
-    setup(name='Cython modules',
-          description="Cython modules for Mercurial (deprecated)",
-          author="Omar Richardson",
-          ext_modules=cythonize(["%s/%s" % (cython_path, module) for module in module_list]))
