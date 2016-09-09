574781aa86fad929531bff4addc939564afd5798
0mar <omsrichardson@gmail.com>
2016-09-09 14:54:38 +0200

    Included potential computer in build structure

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from Cython.Build import cythonize
 
 mde = Extension(name='mde', sources=['src/fortran/mde.f90'])
 micro_macro = Extension(name='micro_macro', sources=['src/fortran/micro_macro.f90'])
+potential_computer = Extension(name='micro_macro', sources=['src/fortran/compute_potential_field.f90'])
 pressure_computer = Extension(name='pressure_computer', sources=['src/fortran/pressure_modules.f90',
                                                                  'src/fortran/sparse_modules.f90'])
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     npsetup(name='FORTRAN modules',
             description="FORTRAN modules for Mercurial",
             author="Omar Richardson",
-            ext_modules=[mde, micro_macro, pressure_computer])
+            ext_modules=[mde, micro_macro,potential_computer, pressure_computer])
 
     # Cython will be removed in the next version
     cython_path = 'src/cython_modules'
