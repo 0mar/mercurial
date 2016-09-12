df955c755d1efdc2c125b3d2b8616042c8280172
0mar <omsrichardson@gmail.com>
2016-09-12 11:12:52 +0200

    Removed python cell potential method

--- src/dynamic_planner.py
+++ src/dynamic_planner.py
@@ -288,57 +288,6 @@ class DynamicPlanner:
                         new_candidate_cells.add(nb_cell)
             return new_candidate_cells
 
-        def compute_potential_old(cell):
-            """
-            Computes the potential in one cell, using potential in neighbouring cells.
-            """
-            # Find the minimal directions along a grid cell.
-            # Assume left and below are best, then overwrite with right and up if they are better
-            neighbour_pots = {direction: np.Inf for direction in ft.DIRECTIONS}
-
-            hor_potential = ver_potential = 0
-            hor_cost = ver_cost = np.Inf
-
-            for direction in ft.DIRECTIONS:
-                normal = ft.DIRECTIONS[direction]
-                # numerical direction
-                nb_cell = (cell[0] + normal[0], cell[1] + normal[1])
-                if not self._exists(nb_cell):
-                    continue
-                pot = potential_field[nb_cell]
-                # potential in that neighbour field
-                if direction == 'right':
-                    face_index = (nb_cell[0] - 1, nb_cell[1])
-                elif direction == 'up':
-                    face_index = (nb_cell[0], nb_cell[1] - 1)
-                    # Unit cost values are defined w.r.t faces, not cells!
-                else:
-                    face_index = nb_cell
-                cost = self.unit_field_dict[opposites[direction]].array[face_index]
-                # Cost to go from there to here
-                neighbour_pots[direction] = pot + cost
-                # total potential
-                if neighbour_pots[direction] < neighbour_pots[opposites[direction]]:
-                    if direction in ft.HORIZONTAL_DIRECTIONS:
-                        hor_potential = pot
-                        hor_cost = cost
-                        # lowest in horizontal direction
-                    elif direction in ft.VERTICAL_DIRECTIONS:
-                        ver_potential = pot
-                        ver_cost = cost
-                        # lowest in vertical direction
-                    else:
-                        raise ValueError("Direction unknown")
-            # Coefficients of quadratic equation
-            a = 1 / hor_cost ** 2 + 1 / ver_cost ** 2
-            b = -2 * (hor_potential / hor_cost ** 2 + ver_potential / ver_cost ** 2)
-            c = (hor_potential / hor_cost) ** 2 + (ver_potential / ver_cost) ** 2 - 1
-
-            D = b ** 2 - 4 * a * c
-            x_high = (2 * c) / (-b - math.sqrt(D))
-            # Might not be obvious, but why we take the largest root is found in report.
-            return x_high
-
         candidate_cells = {cell: compute_potential(cell[0],cell[1], self.grid_dimension[0],self.grid_dimension[1], potential_field,
                                                    self.unit_field_dict['left'].array,self.unit_field_dict['right'].array,
                                                    self.unit_field_dict['up'].array,self.unit_field_dict['down'].array, 9999)
