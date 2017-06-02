1ff84ea91638da53baa1cfab3901e538f71f91ca
0mar <omsrichardson@gmail.com>
2017-06-02 09:24:58 +0200

    Finished source code, testing

--- src/fortran/evolve_smoke.f90
+++ src/fortran/evolve_smoke.f90
@@ -1,5 +1,21 @@
 PROGRAM evolve_smoke
   IMPLICIT NONE
+    INTEGER, parameter ::nx=3
+    INTEGER, parameter ::ny=3
+    REAL (kind=8) :: diff,velo_x, velo_y
+    REAL (kind=8) dx,dy,dt
+  INTEGER, DIMENSION(0:nx-1,0:ny-1):: obstacles
+  REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val
+  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
+
+  diff = 1.0
+  velo_x = 1.0
+  velo_y = -2.4
+  dx = 0.1
+  dy = 0.1
+  dt = 0.2
+  obstacles = reshape((/ 0, 0, 0, 0, 0, 0, 0, 0, 0 /), shape(obstacles))
+   call get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val, a_row,a_col)
 
 END PROGRAM evolve_smoke
 
@@ -8,73 +24,74 @@ SUBROUTINE get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val,
   ! Includes one layer for the boundary
   INTEGER nx,ny,nnz
   REAL (kind=8) :: diff,velo_x, velo_y
-  REAL (kind=8) dx,dy,DATA
+  REAL (kind=8) dx,dy,dt
+  REAL (kind=8), dimension(0:nx*ny-1,0:nx*ny-1) :: a_f
   INTEGER :: i,j,el_counter,el_index, self_el
-  REAL (kind=8) :: l_up,l_down,l_left,l_right
-  INTEGER, DIMENSION(nx,ny):: obstacles
+  REAL (kind=8) :: l_up,l_down,l_left,l_right,l_self
+  INTEGER, DIMENSION(0:nx-1,0:ny-1):: obstacles
   ! Number of nonzero elements: 5 for every cell, 1 for every virtual boundary
-  REAL (kind=8), DIMENSION(0, 5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val
-  INTEGER (kind=8), DIMENSION(0, 5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
-  INTEGER (kind=8), DIMENSION(0, nx*ny-1) :: a_crow
+  REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val
+  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
+  INTEGER (kind=8), DIMENSION(0:nx*ny-1) :: a_crow
   nnz = 5*(nx-2)*(ny-2) + 2*(nx + ny)-4
 
   ! Coefficients
-  l_up = diff*dt/(dy*dy) - v_y*dt/(2*dy)
-  l_down = diff*dt/(dy*dy) + v_y*dt/(2*dy)
-  l_left = diff*dt/(dx*dx) + v_x*dt/(2*dx)
-  l_right = diff*dt/(dx*dx) - v_x*dt/(2*dx)
+  l_up = diff*dt/(dy*dy) - velo_y*dt/(2*dy)
+  l_down = diff*dt/(dy*dy) + velo_y*dt/(2*dy)
+  l_left = diff*dt/(dx*dx) + velo_x*dt/(2*dx)
+  l_right = diff*dt/(dx*dx) - velo_x*dt/(2*dx)
   l_self = -2*diff*dt*(1/(dx*dx) + 1/(dy*dy)) - 1
 
   ! Start filling matrix
   el_counter = 0
-  out_a = 0
-  DO i = 0, nx + 1
-    DO j = 0, ny + 1
+  a_val = 0
+  DO i = 0, nx - 1
+    DO j = 0, ny - 1
       el_index = i + j * nx
-      IF (obstacles(i,j)==1) THEN
-        out_a(el_counter) = 1.d0
+      IF (i==0 .or. i==nx-1 .or. j==0 .or. j==ny-1 .or. obstacles(i,j)==1) THEN
+        a_val(el_counter) = 1.d0
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index
         el_counter = el_counter + 1
       ELSE
         ! Self relation
         self_el = el_counter
-        out_a(el_counter) = l_self
+        a_val(el_counter) = l_self
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index
         el_counter = el_counter + 1
         ! Down relation
         IF (obstacles(i,j-1)==0) THEN
-          out_a(el_counter) = l_down
+          a_val(el_counter) = l_down
         ELSE
-          out_a(self_el) = out_a(self_el) + l_down
+          a_val(self_el) = a_val(self_el) + l_down
         ENDIF
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index - nx
         el_counter = el_counter + 1
         ! Left relation
         IF (obstacles(i-1,j)==0) THEN
-          out_a(el_counter) = l_left
+          a_val(el_counter) = l_left
         ELSE
-          out_a(self_el) = out_a(self_el) + l_left
+          a_val(self_el) = a_val(self_el) + l_left
         ENDIF
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index - 1
         el_counter = el_counter + 1
         ! Right relation
         IF (obstacles(i+1,j)==0) THEN
-          out_a(el_counter) = l_right
+          a_val(el_counter) = l_right
         ELSE
-          out_a(self_el) = out_a(self_el) + l_right
+          a_val(self_el) = a_val(self_el) + l_right
         ENDIF
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index + 1
         el_counter = el_counter + 1
         ! Up relation
         IF (obstacles(i,j+1)==0) THEN
-          out_a(el_counter) = l_up
+          a_val(el_counter) = l_up
         ELSE
-          out_a(self_el) = out_a(self_el) + l_up
+          a_val(self_el) = a_val(self_el) + l_up
         ENDIF
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index + nx
@@ -83,20 +100,27 @@ SUBROUTINE get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val,
     ENDDO
   ENDDO
 
+  a_f = 0
+  do i=0,nnz-1
+      a_f(a_row(i),a_col(i)) = a_val(i)
+  enddo
+  do i=0,nx*ny-1
+    write(*,"100g15.5") ( a_f(i,j), j=0,nx*ny-1 )
+  enddo
 ! CALL compress_row(nx*ny,nnz,a_row,a_crow) no need, lets compress D^-1 & R
 END SUBROUTINE
 
 SUBROUTINE iterate_jacobi(a_val,a_crow,a_col,b,x,nx,ny,rel_els)
   IMPLICIT NONE
-  REAL (kind=8), DIMENSION(0, 5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val, D_inv_val, R_val
-  INTEGER (kind=8), DIMENSION(0, 5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) ::a_row, a_col,d_inv_row, d_inv_col,r_row, r_col
-  INTEGER (kind=8), DIMENSION(0, nx*ny-1) :: a_crow,d_inv_crow,r_inv_crow
-  REAL (kind=8), DIMENSION(0,nx*ny-1) :: b,x,x_old,tmp,rel_els
+  REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val, D_inv_val, R_val
+  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) ::a_row, a_col,d_inv_row, d_inv_col,r_row, r_col
+  INTEGER (kind=8), DIMENSION(0:nx*ny-1) :: a_crow,d_inv_crow,r_crow
+  REAL (kind=8), DIMENSION(0:nx*ny-1) :: b,x,x_old,tmp,rel_els
   INTEGER (kind=8) nx,ny,nnz, iter, max_iter
   INTEGER (kind=8) el_counter_D, el_counter_R, el_counter
   REAL tol, error
 
-  tol = MIN(dx,dy)**2
+  tol = 0.00002
   nnz = 5*(nx-2)*(ny-2) + 2*(nx + ny)-4
   max_iter = 10000
 
@@ -122,7 +146,6 @@ SUBROUTINE iterate_jacobi(a_val,a_crow,a_col,b,x,nx,ny,rel_els)
   ! Apply the jacobi iteration
   x_old = 0
   DO iter = 0, max_iter
-    iter = iter + 1
     error = norm2((x - x_old)*rel_els)
     IF (error < tol) THEN
       EXIT
