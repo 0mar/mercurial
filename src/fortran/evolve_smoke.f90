4e936e9f651d99f68d7ffea68e824f276307e0a6
0mar <omsrichardson@gmail.com>
2017-06-02 14:54:51 +0200

    Tested jacobi iterations

--- src/fortran/evolve_smoke.f90
+++ src/fortran/evolve_smoke.f90
@@ -1,24 +1,39 @@
 PROGRAM evolve_smoke
   IMPLICIT NONE
-    INTEGER, parameter ::nx=4
-    INTEGER, parameter ::ny=4
-    INTEGER :: nnz
+    INTEGER, parameter ::nx=40
+    INTEGER, parameter ::ny=40
+    INTEGER :: nnz,i,j
     REAL (kind=8) :: diff,velo_x, velo_y
+    REAL (kind=8) :: x,y,t,c
+  REAL (kind=8), DIMENSION(0:nx*ny-1) :: b,u,rel_els, f
     REAL (kind=8) dx,dy,dt
   INTEGER, DIMENSION(0:nx-1,0:ny-1):: obstacles
   REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val
-  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
+  INTEGER, DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
 
-  diff = 0.2
-  velo_x = 1.0
-  velo_y = -2.4
+  diff = 0.1
+  velo_x = 0.9
+  velo_y = 1.2
   dx = 0.1
   dy = 0.1
   dt = 0.2
   ! obstacles = reshape((/ 0, 0, 0, 0, 0, 0, 0, 0, 0 /), shape(obstacles))
   obstacles = 0
-  obstacles(1,2) = 1
+  obstacles(1,2) = 0
+
+  c = 2
+  do j=0,ny-1
+      y = j*dy
+      do i=0,nx-1
+       x = i*dx 
+        f(i+j*nx) = c*exp(-((x-nx*dx/2)**2+(y-ny*dy/2)**2)*c**2)
+      enddo
+  enddo
    call get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val, a_row,a_col, nnz)
+   rel_els = 1-reshape(obstacles,shape(rel_els))
+   u=0
+   call iterate_jacobi(a_val,a_row,a_col,f,u,nx,ny,nnz,rel_els)
+  write(*, '(*(F7.3))')( u(j) ,j=0,nx*ny-1)
 
 END PROGRAM evolve_smoke
 
@@ -34,8 +49,8 @@ SUBROUTINE get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val,
   INTEGER, DIMENSION(0:nx-1,0:ny-1):: obstacles
   ! Number of nonzero elements: 5 for every cell, 1 for every virtual boundary
   REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val
-  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
-  INTEGER (kind=8), DIMENSION(0:nx*ny-1) :: a_crow
+  INTEGER , DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4) :: a_col,a_row
+  INTEGER , DIMENSION(0:nx*ny-1) :: a_crow
   max_nnz = 5*(nx-2)*(ny-2) + 2*(nx + ny)-4
 
   ! Coefficients
@@ -51,9 +66,7 @@ SUBROUTINE get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val,
   DO j = 0, ny - 1
     DO i = 0, nx - 1
       el_index = i + j * nx
-      write(*,*) "Index: ",el_index
       IF (i==0 .or. i==nx-1 .or. j==0 .or. j==ny-1 .or. obstacles(i,j)==1) THEN
-        write(*,*) "Obstacle thingie for",i,j
         a_val(el_counter) = 1.d0
         a_row(el_counter) = el_index
         a_col(el_counter) = el_index
@@ -106,24 +119,24 @@ SUBROUTINE get_sparse_matrix(diff,velo_x,velo_y,nx,ny,dx,dy,dt,obstacles, a_val,
   ENDDO
   nnz = el_counter
 
-  a_f = 0
-  do i=0,nnz-1
-      a_f(a_row(i),a_col(i)) = a_val(i)
-  enddo
-  do i=0,nx*ny-1
-      write(*, '(*(F7.3))')( a_f(i,j) ,j=0,nx*ny-1)
-  enddo
+!   a_f = 0
+!   do i=0,nnz-1
+!       a_f(a_row(i),a_col(i)) = a_val(i)
+!   enddo
+!   do i=0,nx*ny-1
+!       write(*, '(*(F7.3))')( a_f(i,j) ,j=0,nx*ny-1)
+!   enddo
 ! CALL compress_row(nx*ny,nnz,a_row,a_crow) no need, lets compress D^-1 & R
 END SUBROUTINE
 
 SUBROUTINE iterate_jacobi(a_val,a_row,a_col,b,x,nx,ny,nnz,rel_els)
   IMPLICIT NONE
   REAL (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) :: a_val, D_inv_val, R_val
-  INTEGER (kind=8), DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) ::a_row, a_col,d_inv_row, d_inv_col,r_row, r_col
-  INTEGER (kind=8), DIMENSION(0:nx*ny-1) :: d_inv_crow,r_crow
+  INTEGER , DIMENSION(0:5*(nx-2)*(ny-2) + 2*(nx + ny)-4-1) ::a_row, a_col,d_inv_row, d_inv_col,r_row, r_col
+  INTEGER , DIMENSION(0:nx*ny-1) :: d_inv_crow,r_crow
   REAL (kind=8), DIMENSION(0:nx*ny-1) :: b,x,x_old,tmp,rel_els
-  INTEGER (kind=8) nx,ny,nnz, iter, max_iter
-  INTEGER (kind=8) el_counter_D, el_counter_R, el_counter
+  INTEGER nx,ny,nnz, iter, max_iter
+  INTEGER  el_counter_D, el_counter_R, el_counter
   REAL tol, error
 
   tol = 0.00002
@@ -149,16 +162,17 @@ SUBROUTINE iterate_jacobi(a_val,a_row,a_col,b,x,nx,ny,nnz,rel_els)
   CALL compress_row(nx*ny, nnz - nx*ny, R_row, R_crow)
 
   ! Apply the jacobi iteration
-  x_old = 0
+  x_old = - 1
   write(*,*) "Starting Iterations"
   DO iter = 0, max_iter
     error = norm2((x - x_old)*rel_els)
     IF (error < tol) THEN
-        write(*,*) "Reached tolerance of ",tol
       EXIT
     ENDIF
+    x_old = x
     CALL sparse_matmul(nx*ny,nnz,r_val,r_crow,r_col,x,tmp)
-    CALL sparse_matmul(nx*ny,nnz,d_inv_val,d_inv_crow,b-tmp,x)
+    CALL sparse_matmul(nx*ny,nnz,d_inv_val,d_inv_crow,d_inv_col,b-tmp,x)
   ENDDO
+    write(*,*) "Reached tolerance of ",error, " in ",iter," iterations"
 
 END SUBROUTINE
