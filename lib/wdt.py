Found bug in computation part of fortran module

lib/wdt.py
@@ -1,5 +1,6 @@
-import numpy as np
 
+import numpy as np
+import time
 try:
     import lib.wdt_module as _wdt
 
@@ -45,7 +46,7 @@ def map_image_to_costs(image):
     cost_field[obstacles] = np.inf
     cost_field[exits] = 0
     cost_field[space] = 1. / (255 * grey_scales[space])
-    return cost_field
+    return cost_field*255
 
 
 def get_weighted_distance_transform(cost_field):
@@ -63,15 +64,19 @@ def get_weighted_distance_transform(cost_field):
         # Fortran does not allow for infinite float.
         nx, ny = cost_field.shape
         # Float that is (probably far) higher than the highest reachable potential
-        obstacle_value = np.max(cost_field[cost_field < np.inf]) * nx * ny * 2
-
+        obstacle_value = 1500  # np.max(cost_field[cost_field < np.inf]) * nx * ny * 2
         cost_field[cost_field == np.inf] = obstacle_value
         # Run the Fortran module
+        time1 = time.time()
         wdt_field = _wdt.weighted_distance_transform(cost_field, nx, ny, obstacle_value)
+        time2 = time.time()
+        print(time2-time1)
         wdt_field[wdt_field >= obstacle_value] = np.inf
         cost_field[cost_field >= obstacle_value] = np.inf
         return wdt_field
     else:
+        # cost_field *= 255.
+        # plot(cost_field)
         # Run python implementation
         return _wdt_python(cost_field)
 

src/micro/potential.py
@@ -3,9 +3,9 @@ import matplotlib
 matplotlib.use('TkAgg')
 import numpy as np
 from math_objects import functions as ft
-from math_objects.geometry import Point
+from scipy.ndimage import gaussian_filter
 from math_objects.scalar_field import ScalarField as Field
-from lib.wdt import get_weighted_distance_transform
+from lib.wdt import get_weighted_distance_transform, plot
 import matplotlib.pyplot as plt
 
 
@@ -27,8 +27,9 @@ class PotentialTransporter:
         self.scene = scene
         self.config = scene.config
         self.show_plot = show_plot
-
-        wdt = get_weighted_distance_transform(self.scene.env_field)
+        cost_field = self._add_obstacle_discomfort(radius=4)
+        wdt = get_weighted_distance_transform(cost_field)
+        plot(wdt)
         self.dx, self.dy = self.scene.size.array / wdt.shape
         self.potential_field = Field(wdt.shape, Field.Orientation.center, 'potential', (self.dx, self.dy))
         self.potential_field.array = wdt
@@ -41,6 +42,16 @@ class PotentialTransporter:
         self.compute_potential_gradient()
         self.grad_x_func = self.pot_grad_x.get_interpolation_function()
         self.grad_y_func = self.pot_grad_y.get_interpolation_function()
+
+    def _add_obstacle_discomfort(self, radius):
+        cost_field = self.scene.env_field.copy()
+        cost_field[cost_field == np.inf] = 0
+        cost_field[self.scene.env_field == np.inf] = 5
+        cost_field = gaussian_filter(cost_field, sigma=radius)
+        cost_field[self.scene.env_field == np.inf] = np.inf
+        cost_field[self.scene.env_field == 0] = 0
+        plot(cost_field)
+        return cost_field
 
     # def _get_fire_effects(self):
     #     fire_effects = np.zeros(self.grid_dimension)